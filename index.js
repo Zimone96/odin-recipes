aa4ad4c845f93194e95d0b8070f6e043c2209b55
Zimone96 <zimonesalome@gmail.com>
2023-11-02 09:49:44 +0200

    added note

---
+++ index.js
@@ -0,0 +1,10 @@
+let welcome = "Please note that these recipes and pictures are not my own and were extracted from other websites, this landing page is a demo of a static website developed by me. 😁";
+prompt = `${welcome}`;
+
+function message (){
+    alert (
+    welcome
+    )
+}
+
+message();
